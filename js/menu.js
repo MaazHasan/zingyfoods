079c2c1fe1f2299e64a3feb28fff70cbbd3dfbd6
Kamal Hasan <>
2020-10-16 23:55:26 +0530

    WIP

---
+++ js/menu.js
@@ -0,0 +1,16 @@
+// import mockData from "./mock-data.json";
+window.onload = () => {
+  const mockData = [
+    { name: "Aloo Tikka Roll", price: 230 },
+    { name: "Veg Haryali Roll", price: 230 },
+    { name: "Paneer Tikka Roll", price: 250 },
+    { name: "Soya Tikka Roll", price: 240 },
+    { name: "Dal Makhani", price: 175 },
+    { name: "Paneer Zaika", price: 260 },
+    { name: "Soya Tawa Masala", price: 230 },
+    { name: "Veg Biryani", price: 220 },
+    { name: "Chicken Tikka Roll", price: 280 },
+    { name: "Chicken Seekh Roll", price: 270 },
+    { name: "Chicken Malai Tikka Roll", price: 300 },
+  ];
+};
